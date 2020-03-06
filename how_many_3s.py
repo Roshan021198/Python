af9c2e0bc3f87e6db5bf65816dfb70573d1c5214
Aditya Kumar <39959261+aditya2082@users.noreply.github.com>
2020-03-06 20:33:50 +0530

    Add file via upload

---
+++ how_many_3s.py
@@ -0,0 +1,13 @@
+"""
+
+@author: Aditya Kumar
+"""
+
+a=int(input("enter number \t"))
+count=0
+while a!=0:
+    x=a%10
+    if(x==3):
+        count+=1
+    a=a//10
+print("numbers of threes = "+str(count))
