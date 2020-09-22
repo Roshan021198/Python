780e50a0a0f53e08f51205bc70934e559b940143
Aditya Kumar <39959261+aditya2082@users.noreply.github.com>
2020-09-22 08:42:31 +0530

    SCRAPING INST PROFILE
    SCRAPING INSTAGRAM PROFILE INFORMATION USING PYTHON

---
+++ InstaProInfo.py
@@ -0,0 +1,28 @@
+'''
+SCRAPING INSTAGRAM PROFILE iNFORMATION USING PYTHON
+
+'''
+
+from bs4 import BeautifulSoup
+import requests
+
+URL = "https://www.instagram.com/{}/"
+def parse_data(s):
+    data = {}
+    s = s.split("-")[0]
+    s = s.split(" ")
+    data['Followers'] = s[0]
+    data['Following'] = s[2]
+    data['Posts'] = s[4]
+    return data
+
+def scrape_data(username):
+    r = requests.get(URL.format(username))
+    s = BeautifulSoup(r.text, "html.parser")
+    meta = s.find("meta", property = "og:description")
+    return parse_data(meta.attrs['content'])
+
+if __name__=="__main__":
+    username = "its_ashmita_"
+    data = scrape_data(username)
+    print(data) 
